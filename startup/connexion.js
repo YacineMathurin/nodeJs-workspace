62f52779c0295b68f7dbb5556dc2adcf2e8c8c6e
Yacine MATHURIn <mathurin_yacine@qenvi.com>
2021-07-30 17:57:05 +0200

    Docker compose file addedd in order to connect to MongoDB container

--- startup/connexion.js
+++ startup/connexion.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const winston = require("winston");
 const config = require("config");
+const dotenv = require('dotenv');
+dotenv.config();
 
 module.exports = function () {
   if (!config.get("jwtPrivateKey")) {
@@ -8,15 +10,22 @@ module.exports = function () {
     winston.error("FATAL ERROR: jwtPrivateKey not set as config !");
     process.exit(1);
   }
+  // if (!process.env.MONGO_IMAGE_IP || !process.env.DB_NAME) {
+  //   console.error("MONGO_IMAGE_IP and DB_NAME should all be set as .env variables");
+  //   process.exit(1);
+  // }
   // Returns a promise
   // mongoose.connect("mongodb://localhost/playground").then(() => {
   mongoose
     .connect(
       // "mongodb+srv://admin:CloudData1@cluster0-pjomp.mongodb.net/test?retryWrites=true&w=majority"
       "mongodb://localhost/blachere"
+      // To connect to Containerized MongoDB, MONGO_IMAGE_IP and DB_NAME to 
+      // be configured in .env file at the root of the project
+      // `mongodb://${process.env.MONGO_IMAGE_IP}:27017/${process.env.DB_NAME}`,
     )
     .then(() => {
-      console.log("Connected ...");
-      winston.info("Connected to database ...");
+      console.log("Successfully Connected to database ...");
+      winston.info("Successfully Connected to database ...");
     });
 };
