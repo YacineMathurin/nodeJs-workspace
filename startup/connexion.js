656e9b8f6da44ae472da10c4b359fb2feeb314a3
Yacine MATHURIn <mathurin_yacine@qenvi.com>
2021-08-05 10:19:26 +0200

    docker updates

--- startup/connexion.js
+++ startup/connexion.js
@@ -31,5 +31,5 @@ module.exports = function () {
         console.log("Mongoose Connected Successfully")
       winston.info("Successfully Connected to database ...");
     })
-    .catch((err) => console.error("Oups, Mongoose Connection failed !"));
+    .catch((err) => console.error("Oups, Mongoose Connection failed !", process.env.MONGO_IMAGE_IP, process.env.DB_NAME));
 };
